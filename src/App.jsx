970848c096324fbca0f78d7d0f98a7c79080c20f
Abotareq <7modetareq@gmail.com>
2025-04-19 20:18:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-200 text-6xl">hello world</h1>
+    </>
+  );
+}
+
+export default App;
