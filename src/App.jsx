101c6901fc8ea31a77c676d2f835becc33437d17
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-04-20 14:13:36 +0200

    adding the componenst to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="bg-slate-200 text-6xl">hello world</h1>
+    <Header />
+    <Hero />
     </>
   );
 }
