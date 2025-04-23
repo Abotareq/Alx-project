8d2fa2b2d9cd2d31a7140a5e283afa647eb3d1c6
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-04-23 06:40:07 +0200

    adding blog page and modify responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,5 @@
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
-export default function Header(){
-    return(
-        <>
-            <header className={styles.header}>
-                <div className={styles.logo}>
-                <Link to="/">
-                    <span>S</span><span>m</span><span>a</span><span>r</span><span>t</span><span>S</span><span>t</span><span>e</span><span>p</span>
-                </Link>
-                 </div>
-        <nav className={styles.nav}>
-            
-            <div className={styles.tgs}>
-            <Link to="/"><a href="#" class={styles.active}>Home</a></Link>
-            <Link to="#">Our Services</Link>
-            <Link to="#">Projects</Link>
-            <Link to="#">Shop</Link>
-            <Link to="/blog"><button className={styles.tgs}>Blog</button></Link>
-            <Link to="#">ContactUs</Link>
-        </div>
-        </nav>
-        <div className={styles.action}>
-            <div className={styles.iconbutton}>
-                🔍
-            </div>
-            <Link to="/signin"><button className={styles.btn}>Login</button></Link>
-            <Link to="/signup"><button className={styles.btn}>Sign Up</button></Link>
-
 
 export default function Header() {
   return (
@@ -54,7 +27,7 @@ export default function Header() {
           {/* Replace these with real <Link>s when routing is ready */}
           <a href="#">Pages</a>
           <a href="#">Our Services</a>
-          <a href="#">Blog</a>
+          <Link to="/blog">Blog</Link>
           <a href="#">Contact Us</a>
         </div>
       </nav>
