99b4c4e67c52087b33428199cfa464b356c3a19b
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-04-26 09:52:48 +0300

    uploaded 4 files

---
+++ src/pages/LearningPage/Catagories.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CategorySideCard from "../../components/CategorySideCard";
+import { catagories } from "../../constants";
+import { catagoriesImages } from "../../constants";
+export default function Catagories() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+        <h1 className="text-2xl font-bold mb-4">Categories</h1>
+        <div className="grid grid-flow-row gap-y-5 w-full max-w-screen-lg">
+          {catagories.map((category, index) => (
+            <CategorySideCard
+              key={index}
+              title={category}
+              imageUrl={catagoriesImages[index]}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
